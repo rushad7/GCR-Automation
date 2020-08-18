a7188236c6fb9d9024a9255422421d902e642d66
Rushad Mehta <rushadmehta16@gmail.com>
2020-08-18 18:53:27 +0530

    Added function to login to GCR.

---
+++ GCR.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 11:16:31 2020
+
+@author: Rushad
+"""
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementNotInteractableException, StaleElementReferenceException, NoSuchElementException
+
+gcr_url = "https://accounts.google.com/signin/v2/identifier?service=classroom&passive=1209600&continue=https%3A%2F%2Fclassroom.google.com%2Fu%2F0%2Fh&followup=https%3A%2F%2Fclassroom.google.com%2Fu%2F0%2Fh&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+
+DRIVER_PATH = r"C:/chromedriver.exe"
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+
+email = "rb3049@srmist.edu.in"
+password = "SRMIST@#8930412"
+
+def login(email, password):
+    
+    try:
+    
+        driver.get(gcr_url)
+        
+        email_id = driver.find_element_by_name('identifier')
+        email_id.send_keys(email)
+        submit_email_id = driver.find_element_by_class_name('VfPpkd-RLmnJb')
+        submit_email_id.click()
+        
+        password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'password')))
+        pswd = password_field.find_element_by_tag_name('input')
+        pswd.send_keys(password)
+        submit_password = driver.find_element_by_class_name('VfPpkd-RLmnJb')
+        submit_password.click()
+    
+        print("Login successfull \n")
+        print("Logged in as {}".format(email))
+    
+    except ElementNotInteractableException or StaleElementReferenceException or NoSuchElementException:
+        print("Login Failed")
+    
+login(email, password)
